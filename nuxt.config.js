0f0f41962dc2317de6e2ebbc3ac46343559194c5
Marichka Offen <marichka.offen@gmail.com>
2021-09-18 21:36:41 -0400

    Updated nuxt config with meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,17 @@
 export default {
-  components: true // not necessary to import and define components anymore
+  components: true, // not necessary to import and define components anymore
+  head: {
+    titleTemplate: "Mastering Nuxt: %s",
+    htmlAttrs: {
+      lang: "en"
+    },
+    bodyAttrs: {
+      class: ["my-style"]
+    },
+    meta: [
+      {
+        charset: "utf-8"
+      }
+    ]
+  }
 }
